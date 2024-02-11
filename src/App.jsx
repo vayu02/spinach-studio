f0b8423cd490762f13a7a7a815ecf3d7dc7a271a
vayu02 <ashish241.shejwal@gmail.com>
2024-02-12 02:32:57 +0530

    initialized react.js with vite added eslint, prettier and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <main className="w-screen h-screen grid place-content-center">
+            <h1>Hello World</h1>
+        </main>
+    )
+}
+
+export default App
